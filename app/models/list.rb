949b235c12d4b419b3f6d5fadb149f0be9341e65
hirokimac <hiroki.yokomizo1030@gmail.com>
2020-12-24 21:55:25 +0900

    Rails8_end

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,2 +1,6 @@
 class List < ApplicationRecord
 end
+
+class List < ApplicationRecord
+  attachment :image
+end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'e1fd654a32baa92f6febc4d9e78bc4634a593776071f143c3cda1ea78654ed52342959bdcafa3eb65c2d34ecb850a0a65e7f490da1a2bef2bba821d547df78b4'
